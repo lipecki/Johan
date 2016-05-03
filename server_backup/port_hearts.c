98de66bd9c8b6d2c50cc7133c7c145d0ee927fa6
lipecki <lipecki@kth.se>
2016-05-03 09:02:30 +0200

    tillbaka till framtiden
    syn-acken har fått tillbaka sin fork() och stänger inga sockets i
    loppen kring recv(). Den har också fått tillbaka if(!done) innan
    syn-acken och efter recv()

---
+++ server_backup/port_hearts.c
@@ -0,0 +1,88 @@
+//
+//  port_hearts.c
+//  
+//
+//  Created by Johan Lipecki on 2016-04-20.
+//
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <pwd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include "port_hearts.h"
+
+
+#define PARENT 1
+
+int start_game_server(int players, int port){
+     /* forka processen när spelare 0 ansluter*/
+    if(!players){
+        if(!(fork())){
+            //Executing as child process
+            syslog(LOG_INFO,"Starting Game Server");
+            execlp("bin/sh","sh","-c",GAME_SERVER,port,NULL);
+            syslog(LOG_ERR,"%s",strerror(errno));
+        }
+        else {
+            wait(0);
+            return 0;
+            //no waiting around for child? zombie creator?
+        }
+    }
+    else return port;
+}
+int get_random_port_number(void){
+    srandom(time(NULL));
+    return (random()%10000 + 40000);
+}
+int syn_ack(char* arguments,int syn,int fd){
+    pid_t child_pid;
+    int static port;
+    /* Duplicate this process. */
+    child_pid = fork ();
+    if(child_pid != 0){
+        /* This is the parent process. */
+        close(fd);
+        close(1);
+        wait(0);
+        return 1;
+    }
+    else {
+        //child
+        //Redirect stdout to socket
+        close(1);
+        dup(fd);
+        //SYN-ACK switch
+        if(!strcmp(arguments,SYN0)){
+            syslog(LOG_INFO,"argument: %s", arguments);
+            if(!syn) strcpy(arguments,ACK0);
+            syslog(LOG_INFO,"argument: %s", arguments);
+        }
+        else if(!strcmp(arguments,SYN1) && syn){
+            syslog(LOG_INFO,"argument: %s, SYN1: %s", arguments, SYN1);
+            return 2;
+        }
+        else strcpy(arguments,"it's the ping of death for you my friend!");
+        /* Now execute the commands in a new session*/
+        execlp("/bin/sh","bash","-c", "echo" ,arguments, NULL);
+        /* The execlp function returns only if an error occurs. */
+        syslog(LOG_ERR,"%s",strerror(errno));
+        abort();
+    }
+    syslog(LOG_INFO, "Syn-ack end reached");
+    return 0;
+}
+
---
+++ server_backup/port_hearts.h
@@ -0,0 +1,27 @@
+//
+//  port_hearts.h
+//  
+//
+//  Created by Johan Lipecki on 2016-04-20.
+//
+//
+
+#ifndef port_hearts_h
+#define port_hearts_h
+
+#define SYN0 "hearts"
+#define ACK0 "diamonds"
+#define SYN1 "port"
+
+#define GAME_SERVER "./game_server"
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define IP_ADDRESS "192.168.0.2"
+
+#include <stdio.h>
+int start_game_server(int,int); //
+int syn_ack(char *,int,int);
+int get_random_port_number(void);
+
+#endif /* port_hearts_h */
