7199b0af6f69493c8768a291716be4ce4d323ea5
lipecki <lipecki@kth.se>
2016-05-04 13:52:52 +0200

    syn-ack header

---
+++ client_backup/syn-ack-client.h
@@ -0,0 +1,27 @@
+//
+//  syn-ack-client.h
+//  
+//
+//  Created by Johan Lipecki on 2016-05-04.
+//
+//
+
+#ifndef syn_ack_client_h
+#define syn_ack_client_h
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <SDL2/SDL.h>
+#include <SDL2_net/SDL_net.h>
+
+#define PORT 1337
+#define MAXLEN 1024
+#define IP_ADDRESS "130.237.84.89"
+#define GAME_CLIENT "game_client "
+#define ERROR_LOG "/var/tmp/hearts_error_log"
+
+#endif /* syn_ack_client_h */
