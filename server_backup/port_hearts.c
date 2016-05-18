f83b9473508495bb74813e6806b00aeaabc646a8
lipecki <lipecki@kth.se>
2016-05-18 11:10:09 +0200

    LOG_ERR ENDOFTRANS

--- server_backup/port_hearts.c
+++ server_backup/port_hearts.c
@@ -41,7 +41,10 @@ int syn_ack(char* arguments, int *i,int sd, int port, int connection_no){
         }
         syslog(LOG_INFO,"Exiting syn-ack()");
     }
-    else return 1;
+    else{
+        syslog(LOG_ERR,"ENDOFTRANS");
+        return 1;
+    }
     return 0;
 }
 Account prompt_for_login(int *socketDescriptor) {
