4b5d8c488e58a4f9c10da62edd3788055c0a4120
lipecki <lipecki@kth.se>
2016-05-18 22:29:10 +0200

    tar bort hängslena

--- server_backup/servit/server_backup/kill_Inet.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo $(pgrep Inet)
-sudo kill -15 $(pgrep Inet)
-echo $(pgrep Inet)
--- server_backup/servit/server_backup/kill_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo $(pgrep server)
-sudo kill -15 $(pgrep server)
-echo $(pgrep server)
-cat /var/log/syslog | grep server
